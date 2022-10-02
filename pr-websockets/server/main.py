17c7bf0445e78e5739a856075594a26c69e0ffab
sanghoho <ash1151@ajou.ac.kr>
2022-10-02 19:31:11 +0900

    PR-WebSockets/UPDATE : Server WebSocket connection mananger updated

--- pr-websockets/server/main.py
+++ pr-websockets/server/main.py
@@ -1,63 +1,32 @@
+import uvicorn
+
+from typing import List
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 
-import uvicorn
-
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(    ) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+class ConnectionManager:
+    def __init__(self):
+        self.connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+
+    async def broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
 
 
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
+manager = ConnectionManager()
 
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-
-    print('CONNECTING...')
-    await websocket.accept()
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
     while True:
-        try:
-            data = await websocket.receive_text()
-            await websocket.send_text(f"Message text was: {data}")
-        except Exception as e:
-            print(e)
-            break
-    print("CONNECTION DEAD...")
+        data = await websocket.receive_text()
+        await manager.broadcast(f"Client {client_id}: {data}")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
